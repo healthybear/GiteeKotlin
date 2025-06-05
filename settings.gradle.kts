80c489f11a14c0eff965cf7ff3b8df3e4d78e457
healthybear <1205893858@qq.com>
2025-06-05 23:16:20 +0800

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        maven(uri("https://maven.aliyun.com/repository/central"))
+        maven(uri("https://maven.aliyun.com/repository/public"))
+        maven(uri("https://maven.aliyun.com/repository/gradle-plugin"))
+        maven(uri("https://maven.aliyun.com/repository/google"))
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven(uri("https://maven.aliyun.com/repository/central"))
+        maven(uri("https://maven.aliyun.com/repository/public"))
+        maven(uri("https://maven.aliyun.com/repository/gradle-plugin"))
+        maven(uri("https://maven.aliyun.com/repository/google"))
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GiteeKotlin"
+include(":app")
